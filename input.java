c71ce34d8506dedbb82fc7c93ad5c5ddbea5ac01
Avinash-Choudhary2004 <129417495+Avinash-Choudhary2004@users.noreply.github.com>
2023-03-30 23:31:34 +0530

    Add files via upload

--- input.java
+++ input.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;  
 
 class Main {
+    public static int sum(int num1, int num2){
+        int sum=num1+num2;
+        return sum;
+    }
   public static void main(String[] args) {
     Scanner num = new Scanner(System.in);  
     System.out.println("Enter number");
 
-    int number = num.nextInt();  
+    int num1 = num.nextInt();
+    System.out.println("Enter other number");
+    int num2=num.nextInt();  
     num.close();
-    if(number>0){
-        System.out.println("The number is positive");
-    }
-    else{
-        System.out.println("The number is negative");
-    }
+    int ans=sum(num1,num2);
+    System.out.println("The sum of given numbers is ");
+    System.out.println(ans);
+    
   }
 }
